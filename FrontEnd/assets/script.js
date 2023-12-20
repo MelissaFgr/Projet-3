ff1a9679eb5ff49e302821d4d803708a04719c4e
Melissa Faugere <melissafaugere@gmail.com>
2023-12-20 17:55:03 +0100

    Test boutons filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,35 +1,39 @@
+//Récupère les infos de chaque projets via l'API pour en afficher l'image et le titre  
 async function afficherWorks() {
     let résultat = await fetch("http://localhost:5678/api/works"); 
     let données = await résultat.json();
     
     for (let i = 0; i < données.length; i++) {
-        const figure = document.createElement("figure");
+        const figureworks = document.createElement("figure");
         const image = document.createElement("img");
         const titre = document.createElement ("figcaption");
 
         image.src = données[i].imageUrl;
         titre.textContent = données[i].title;
         
-        figure.appendChild(image);
-        figure.appendChild(titre);
+        figureworks.appendChild(image);
+        figureworks.appendChild(titre);
 
-        document.querySelector(".gallery").appendChild(figure);
+        document.querySelector(".gallery").appendChild(figureworks);
     }
 
-    let boutonFiltre = document.getElement ("boutonFiltre");
-    boutonFiltre.addEventListener("click", function(event) {
-        let valeurBouton = event.target.value;
-            for(let j = 0; j < données.length; j++) {
+//Fonction filtrage en cours    
+    let boutonFiltre = document.querySelector (".filtered"); //cherche les boutons dans HTML
+    const figureFiltre = document.querySelectorAll ("figure"); //cherche toutes les figures dans le HTML
+    boutonFiltre.addEventListener("click", function(event) { //écoute des boutons
+        let valeurBouton = event.target.value; 
+        console.log (valeurBouton)
+            for(let j = 0; j < données.length; j++) { //devrait cacher l'image sur Id diff de la valeur du bouton (ne fonctionne pas)
                 if(valeurBouton != données[j].categoryId) {
-                    figure.classlist.add("displayNone");
+                    figureFiltre.style.display = "none";  // classlist.add("displayNone") - className("displayNone")
                 }else {
-                    figure.classlist.remove("displayNone");
+                    figureFiltre.style.display = "inline-block"; // classlist.remove("displayNone")
                 }
             }
     })
 }
   
-
+//Récupère les catégories via l'API et création des boutons
 async function afficherCategories() {
     let résultat = await fetch("http://localhost:5678/api/categories"); 
     let données = await résultat.json();
