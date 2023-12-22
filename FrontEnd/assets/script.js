1bad6b540183425849746803a55d3b6dc676e719
Melissa Faugere <melissafaugere@gmail.com>
2023-12-22 14:17:59 +0100

    Début formulaire Log In

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,15 +1,16 @@
 //Récupère les infos de chaque projets via l'API pour en afficher l'image et le titre  
 async function afficherWorks() {
-    let résultat = await fetch("http://localhost:5678/api/works"); 
-    let données = await résultat.json();
+    let resultat = await fetch("http://localhost:5678/api/works"); 
+    let donnees = await resultat.json();
     
-    for (let i = 0; i < données.length; i++) {
+    for (let i = 0; i < donnees.length; i++) {
         const figureworks = document.createElement("figure");
         const image = document.createElement("img");
         const titre = document.createElement ("figcaption");
 
-        image.src = données[i].imageUrl;
-        titre.textContent = données[i].title;
+        image.src = donnees[i].imageUrl;
+        titre.textContent = donnees[i].title;
+        figureworks.setAttribute('id',donnees[i].categoryId);
         
         figureworks.appendChild(image);
         figureworks.appendChild(titre);
@@ -18,31 +19,37 @@ async function afficherWorks() {
     }
 
 //Fonction filtrage en cours    
-    let boutonFiltre = document.querySelector (".filtered"); //cherche les boutons dans HTML
-    const figureFiltre = document.querySelectorAll ("figure"); //cherche toutes les figures dans le HTML
-    boutonFiltre.addEventListener("click", function(event) { //écoute des boutons
-        let valeurBouton = event.target.value; 
-        console.log (valeurBouton)
-            for(let j = 0; j < données.length; j++) { //devrait cacher l'image sur Id diff de la valeur du bouton (ne fonctionne pas)
-                if(valeurBouton != données[j].categoryId) {
-                    figureFiltre.style.display = "none";  // classlist.add("displayNone") - className("displayNone")
-                }else {
-                    figureFiltre.style.display = "inline-block"; // classlist.remove("displayNone")
-                }
-            }
-    })
+let boutonFiltre = document.querySelector(".filtered");
+const figuresFiltre = document.querySelectorAll("figure");
+ 
+boutonFiltre.addEventListener("click", function (event) {
+    let valeurBouton = event.target.value;
+    console.log(valeurBouton);
+ 
+    for (let j = 0; j < figuresFiltre.length; j++) {
+        let figures = figuresFiltre[j];
+ 
+        let figureId = figures.id;
+ 
+        if ( valeurBouton === "Tous" || valeurBouton === figureId) {
+            figures.style.display = "inline-block";
+        } else {
+            figures.style.display = "none";
+        }
+    }
+});
 }
   
 //Récupère les catégories via l'API et création des boutons
 async function afficherCategories() {
-    let résultat = await fetch("http://localhost:5678/api/categories"); 
-    let données = await résultat.json();
+    let resultat = await fetch("http://localhost:5678/api/categories"); 
+    let donnees = await resultat.json();
 
-    for (let i = 0; i < données.length; i++) {
+    for (let i = 0; i < donnees.length; i++) {
         const button = document.createElement("button");
 
-        button.textContent = données[i].name;
-        button.value = données [i].id;
+        button.textContent = donnees[i].name;
+        button.value = donnees [i].id;
 
         document.querySelector(".filtered").appendChild(button);
     }
