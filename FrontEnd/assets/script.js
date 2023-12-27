f937cbbba39c81fce9be49f89bcee03ff60b4c0d
Melissa Faugere <melissafaugere@gmail.com>
2023-12-27 18:33:45 +0100

    Authentification

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,37 +1,37 @@
 //Récupère les infos de chaque projets via l'API pour en afficher l'image et le titre  
-async function afficherWorks() {
-    let resultat = await fetch("http://localhost:5678/api/works"); 
-    let donnees = await resultat.json();
+async function displayWorks() {
+    let result = await fetch("http://localhost:5678/api/works"); 
+    let data = await result.json();
     
-    for (let i = 0; i < donnees.length; i++) {
-        const figureworks = document.createElement("figure");
-        const image = document.createElement("img");
-        const titre = document.createElement ("figcaption");
+    for (let i = 0; i < data.length; i++) {
+        let figureworks = document.createElement("figure");
+        let image = document.createElement("img");
+        let title = document.createElement ("figcaption");
 
-        image.src = donnees[i].imageUrl;
-        titre.textContent = donnees[i].title;
-        figureworks.setAttribute('id',donnees[i].categoryId);
+        image.src = data[i].imageUrl;
+        title.textContent = data[i].title;
+        figureworks.setAttribute('id',data[i].categoryId);
         
         figureworks.appendChild(image);
-        figureworks.appendChild(titre);
+        figureworks.appendChild(title);
 
         document.querySelector(".gallery").appendChild(figureworks);
     }
 
-//Fonction filtrage en cours    
-let boutonFiltre = document.querySelector(".filtered");
-const figuresFiltre = document.querySelectorAll("figure");
+//Fonction filtrage   
+let buttonfilter = document.querySelector(".filtered");
+const figuresFilter = document.querySelectorAll("figure");
  
-boutonFiltre.addEventListener("click", function (event) {
-    let valeurBouton = event.target.value;
-    console.log(valeurBouton);
+buttonfilter.addEventListener("click", function (event) {
+    let valeurButton = event.target.value;
+    console.log(valeurButton);
  
-    for (let j = 0; j < figuresFiltre.length; j++) {
-        let figures = figuresFiltre[j];
+    for (let j = 0; j < figuresFilter.length; j++) {
+        let figures = figuresFilter[j];
  
         let figureId = figures.id;
  
-        if ( valeurBouton === "Tous" || valeurBouton === figureId) {
+        if (valeurButton === "Tous" || valeurButton === figureId) {
             figures.style.display = "inline-block";
         } else {
             figures.style.display = "none";
@@ -41,20 +41,66 @@ boutonFiltre.addEventListener("click", function (event) {
 }
   
 //Récupère les catégories via l'API et création des boutons
-async function afficherCategories() {
-    let resultat = await fetch("http://localhost:5678/api/categories"); 
-    let donnees = await resultat.json();
+async function displayCategories() {
+    let result = await fetch("http://localhost:5678/api/categories"); 
+    let data = await result.json();
 
-    for (let i = 0; i < donnees.length; i++) {
-        const button = document.createElement("button");
+    for (let i = 0; i < data.length; i++) {
+        let button = document.createElement("button");
 
-        button.textContent = donnees[i].name;
-        button.value = donnees [i].id;
+        button.textContent = data[i].name;
+        button.value = data [i].id;
 
         document.querySelector(".filtered").appendChild(button);
     }
 }
 
-afficherCategories()
-afficherWorks()
+displayCategories()
+displayWorks()
+
+function emailCheck(email) {
+    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+}
+//Envoie les données à l'API
+let mailLogin = document.querySelector("#mailLogin")
+let passwordLogin = document.querySelector("#passwordLogin")
+let buttonSubmit = document.querySelector("#submitLogin")
+
+buttonSubmit.addEventListener("click", function (event) {
+    event.preventDefault();
+    var testMail = emailCheck(mailLogin.value)
+    if(testMail === true) {
+        const connection = {
+            email: mailLogin.value,
+            password: passwordLogin.value,
+        };
+        const dataConnection = JSON.stringify(connection);
+    
+        fetch("http://localhost:5678/api/login",{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: dataConnection             
+        })
+        .then(function (response) {
+            if(response.ok) {
+                return response.json();
+            } else {
+                let errorConnection = document.createElement("p");
+                errorConnection.textContent = "Adresse mail ou mot de passe incorrect";
+                document.querySelector("#formLogin").appendChild(errorConnection);
+            }
+        })
+        //récup token en cours
+        .then(function (dataResponse) {
+            var token = dataResponse.token
+            localStorage.setItem("token", token);
+        });
+
+    } else {
+        let errorMessage = document.createElement("p");
+        errorMessage.textContent = "Format de l'e-mail incorrect";
+        document.querySelector("#formLogin").appendChild(errorMessage);
+    }
+});
 
